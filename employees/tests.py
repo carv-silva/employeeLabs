d4ba8010c6475743157b9ae91f9154f450c90daa
vinicius <car_silva@hotmail.com>
2021-04-26 18:33:37 -0300

    unit testing

--- employees/tests.py
+++ employees/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from employees.models import Employee
 
-# Create your tests here.
+
+class empemployeeTest(TestCase):
+    # cria um funcionario padrão
+    def setUp(self):
+        Employee.objects.create(
+            name='carlos', email='teste@gmail.com', department='NOC')
+        Employee.objects.create(
+            name='vinicius', email='vinie@gmail.com', department='help-desk')
+
+    def test_employee_info(info):
+        print('testing Employee Information')
+        funcionarioOne = Employee.objects.get(
+            name='carlos', email='teste@gmail.com', department='NOC')
+        funcionarioTwo = Employee.objects.get(
+            name='vinicius', email='vinie@gmail.com', department='help-desk')
+
